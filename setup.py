2ca4ee09ba6af6477f97840f171676f5a40e5852
token631 <67794936+token631@users.noreply.github.com>
2021-03-20 19:11:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from distutils.core import setup
+
 setup(
   name = 'RBX.py',        
-  packages = ['RBX.py'],   
-  version = '0.1',    
+  packages = ['RBX_py'],   
+  version = '0.0.1',    
   license='MIT',       
   description = 'A simple API wrapper for ROBLOX',  
   author = 'Token',                   
   author_email = 'c@tokyran.com',     
   url = 'https://github.com/user/RBX.py',   
-  download_url = 'https://github.com/user/RBX.py/archive/v_01.tar.gz',   
+  download_url = 'https://github.com/token631/RBX.py/archive/v_01.tar.gz',   
   keywords = ['Roblox'],   
   install_requires=[],
+  long_description="Please view the GitHub page (project Homepage) for more information.",
   classifiers=[
     'Development Status :: 3 - Alpha',     
     'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: MIT License',   
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.6',
   ],
 )
