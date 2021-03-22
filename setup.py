f3227791ff21dc2a9d2886b892d4cc7dfa3cf9ec
KILR <65610641+KILR007@users.noreply.github.com>
2021-03-22 07:47:50 +0000

    Some changes
    - Corrected GitHub URL
    - Keywords Added
    -  install_requires fixed

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
   description = 'A simple API wrapper for ROBLOX',  
   author = 'Token',                   
   author_email = 'c@tokyran.com',     
-  url = 'https://github.com/user/RBX.py',   
+  url = 'https://github.com/token631/RBX.py',   
   download_url = 'https://github.com/token631/RBX.py/archive/v_01.tar.gz',   
-  keywords = ['Roblox'],   
-  install_requires=[],
+  keywords = ['Roblox','roblox api'],   
+  install_requires=['requests'],
   long_description="Please view the GitHub page (project Homepage) for more information.",
   classifiers=[
     'Development Status :: 3 - Alpha',     
