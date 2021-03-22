3914a7dfa774f60c614abb1a9311567dde9055c9
token631 <67794936+token631@users.noreply.github.com>
2021-03-22 17:33:40 +0000

    Added player support

--- RBX.py/Client.py
+++ RBX.py/Client.py
@@ -15,3 +15,15 @@ class Client:
         asset_obj = Asset(id=id)
         asset_obj.fetchData()
         return asset_obj
+    
+    def FetchUser(self, id):
+        """
+        Creates an instance of a Player class using the given ID
+        """
+        TypeErrorCheck = isinstance(id, str)
+        if TypeErrorCheck:
+            raise TypeError(f"User ID ({id}) must be in integer form")
+
+        player_obj = Player(id=id)
+        player_obj.fetchData()
+        return player_obj
