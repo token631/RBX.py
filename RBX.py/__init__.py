97ab62cc53933007124fffba3aad799fed1c3919
token631 <67794936+token631@users.noreply.github.com>
2021-03-22 17:57:16 +0000

    Update __init__.py

--- RBX.py/__init__.py
+++ RBX.py/__init__.py
@@ -4,7 +4,7 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 Token'
 __version__ = '0.5'
 
-from Utils import *
-from Asset import *
-from Errors import *
-from Client import *
+from RBX_py.Utils import *
+from RBX_py.Asset import *
+from RBX_py.Errors import *
+from RBX_py.Client import *
